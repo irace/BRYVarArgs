4759827e94561bd74a9878c72753526655ddef64
Bryan Irace <bryan.irace@gmail.com>
2014-01-03 15:49:03 -0500

    Initial Commit

---
+++ BRYVarArgs/BRYVarArgs.h
@@ -0,0 +1,13 @@
+//
+//  BRYVarArgs.h
+//  BRYVarArgs
+//
+//  Created by Bryan Irace on 1/3/14.
+//  Copyright (c) 2014 Bryan Irace. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BRYVarArgs : NSObject
+
+@end
---
+++ BRYVarArgs/BRYVarArgs.m
@@ -0,0 +1,13 @@
+//
+//  BRYVarArgs.m
+//  BRYVarArgs
+//
+//  Created by Bryan Irace on 1/3/14.
+//  Copyright (c) 2014 Bryan Irace. All rights reserved.
+//
+
+#import "BRYVarArgs.h"
+
+@implementation BRYVarArgs
+
+@end
